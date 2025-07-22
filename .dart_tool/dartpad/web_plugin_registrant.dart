f477f93c0a0c50e26b9aa86f63c9cc382163f7d6
Luis Ruiz <luisruizmalaga@gmail.com>
2025-07-22 16:37:29 +0200

    some version fixxes

---
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -0,0 +1,26 @@
+// Flutter web plugin registrant file.
+//
+// Generated file. Do not edit.
+//
+
+// @dart = 2.13
+// ignore_for_file: type=lint
+
+import 'package:device_info_plus/src/device_info_plus_web.dart';
+import 'package:flutter_dropzone_web/flutter_dropzone_plugin.dart';
+import 'package:flutter_secure_storage_web/flutter_secure_storage_web.dart';
+import 'package:libphonenumber_web/libphonenumber_web.dart';
+import 'package:syncfusion_pdfviewer_web/pdfviewer_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  DeviceInfoPlusWebPlugin.registerWith(registrar);
+  FlutterDropzonePlugin.registerWith(registrar);
+  FlutterSecureStorageWeb.registerWith(registrar);
+  LibPhoneNumberPlugin.registerWith(registrar);
+  SyncfusionFlutterPdfViewerPlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
